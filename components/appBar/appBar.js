e767e7867734b8feaffcdc3cdfa8f5d73ed52c3f
Rasha Faheem <64769011+rashafaheem@users.noreply.github.com>
2021-08-12 06:41:17 -0700

    AppBar

--- components/appBar/appBar.js
+++ components/appBar/appBar.js
@@ -1,47 +1,58 @@
-import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Slide from "@material-ui/core/Slide";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: "rgb(255, 237, 247)",
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-function AppHeader() {
-  const classes = useStyles();
+function HideOnScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
   );
 }
 
-export default AppHeader;
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  window: PropTypes.func,
+};
+
+export default function HideAppBar(props) {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar>
+          <Toolbar className="toolbar">
+            <img className="logo" src="/logo.png" alt="logo" />
+            <h1 className="grapeslab">grapeslab</h1>
+            <div className="headerItems">
+              <a className="headerMenu">Menu</a>
+              <a className="headerMenu">Offers</a>
+              <a className="headerMenu">Foods</a>
+              <a className="headerMenu">Services</a>
+              <a className="headerMenu">Restaurants</a>
+            </div>
+            <FavoriteIcon className="favIcon" />
+            <ShoppingCartIcon className="cartIcon" />
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
+      {/* <Container>
+        <Box my={2}>
+         
+        </Box>
+      </Container> */}
+    </React.Fragment>
+  );
+}
