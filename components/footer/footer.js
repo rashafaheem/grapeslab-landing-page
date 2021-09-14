c09f9f1a42d8775c7323133c7472bac4f850a438
Rasha Faheem <64769011+rashafaheem@users.noreply.github.com>
2021-09-14 08:23:29 -0700

    fix: footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -6,105 +6,95 @@ function Footer() {
   return (
     <div className="footer">
       <Grid container className="footerGrid">
-        <Grid item xs={12} lg={4} className="firstColumn">
-          <div className="firstColumnDiv">
-            <img className="Flogo" src="/logo.png" alt="logo" />
-            <h1 className="Fgrapeslab">grapeslab</h1>
-            <p className="Fsubtext">
-              Lorem ipsum dolor sit amet <br />
-              consectetur adipiscing elit.
-            </p>
-            <div className="socialLogos">
-              <a>
-                <img className="fb" src="./fb.png"></img>
-              </a>
-              <a>
-                <img className="twitter" src="./twitter.png"></img>
-              </a>
-              <a>
-                <img className="instagram" src="./instagram.png"></img>
-              </a>
-              <a>
-                <img className="linkedIn" src="./linkedIn.png"></img>
-              </a>
-            </div>
-          </div>
-        </Grid>
-        <Grid item xs={12} lg={2} className="secondColumnGrid">
-          <div className="secondColumn">
-            <h3 className="h3">Support</h3>
+        <Grid item xs={12} md={4} className="firstColumn">
+          <img className="Flogo" src="/logo.png" alt="logo" />
+          <h1 className="Fgrapeslab">grapeslab</h1>
+          <p className="Fsubtext">
+            Lorem ipsum dolor sit amet <br />
+            consectetur adipiscing elit.
+          </p>
+          <div className="socialLogos">
             <a>
-              <p className="Fsubtext">Account</p>
+              <img className="fb" src="./fb.png"></img>
             </a>
             <a>
-              <p className="Fsubtext">Support Center</p>
+              <img className="twitter" src="./twitter.png"></img>
             </a>
             <a>
-              <p className="Fsubtext">Feedback</p>
+              <img className="instagram" src="./instagram.png"></img>
             </a>
             <a>
-              <p className="Fsubtext">Accebility</p>
+              <img className="linkedIn" src="./linkedIn.png"></img>
             </a>
           </div>
         </Grid>
-        <Grid item xs={12} lg={2} className="secondColumnGrid">
-          <div className="thirdColumn">
-            <h3 className="h3">Our Menu</h3>
-            <a>
-              <p className="Fsubtext">Special</p>
-            </a>
-            <a>
-              <p className="Fsubtext">Popular</p>
-            </a>
-            <a>
-              <p className="Fsubtext">Categories</p>
-            </a>
-          </div>
+        <Grid item xs={12} md={2}>
+          <h3 className="h3">Support</h3>
+          <a>
+            <p className="Fsubtext">Account</p>
+          </a>
+          <a>
+            <p className="Fsubtext">Support Center</p>
+          </a>
+          <a>
+            <p className="Fsubtext">Feedback</p>
+          </a>
+          <a>
+            <p className="Fsubtext">Accebility</p>
+          </a>
         </Grid>
-        <Grid item xs={12} lg={2} className="secondColumnGrid">
-          <div className="fourthColumn">
-            <h3 className="h3">Useful Links</h3>
-            <a>
-              <p className="Fsubtext">Payment & Tax</p>
-            </a>
-            <a>
-              <p className="Fsubtext">Terms of Service</p>
-            </a>
-            <a>
-              <p className="Fsubtext">Privacy Policy</p>
-            </a>
-            <a>
-              <p className="Fsubtext">About Us</p>
-            </a>
-          </div>
+        <Grid item xs={12} md={2}>
+          <h3 className="h3">Our Menu</h3>
+          <a>
+            <p className="Fsubtext">Special</p>
+          </a>
+          <a>
+            <p className="Fsubtext">Popular</p>
+          </a>
+          <a>
+            <p className="Fsubtext">Categories</p>
+          </a>
         </Grid>
-        <Grid item xs={12} lg={2} className="secondColumnGrid">
-          <div className="fifthColumn">
-            <h3 className="h3">Get in touch</h3>
-            <a>
-              <p className="Fsubtext">hellograpeslab@gmail.com</p>
-            </a>
-            <a>
-              <p className="Fsubtext">Sylhet, Bangladesh</p>
-            </a>
-          </div>
+        <Grid item xs={12} md={2}>
+          <h3 className="h3">Useful Links</h3>
+          <a>
+            <p className="Fsubtext">Payment & Tax</p>
+          </a>
+          <a>
+            <p className="Fsubtext">Terms of Service</p>
+          </a>
+          <a>
+            <p className="Fsubtext">Privacy Policy</p>
+          </a>
+          <a>
+            <p className="Fsubtext">About Us</p>
+          </a>
         </Grid>
-        <Grid item xs={12} lg={12}>
+        <Grid item xs={12} md={2}>
+          <h3 className="h3">Get in touch</h3>
+          <a>
+            <p className="Fsubtext">hellograpeslab@gmail.com</p>
+          </a>
+          <a>
+            <p className="Fsubtext">Sylhet, Bangladesh</p>
+          </a>
+        </Grid>
+        <Grid item xs={12} md={12}>
           <div className="divider">
             <Divider className="divider" />
           </div>
         </Grid>
-        <Grid container spacing={3}>
-          <Grid item xs={12} lg={6}>
-            <p className="copyright">© 2021 Grapeslab. All rights reserved.</p>
-          </Grid>
-          <Grid item xs={12} lg={6}>
-            <a>
-              <img className="creditcard" src="./cc1.png"></img>
-            </a>
-          </Grid>
+        {/* <Grid container spacing={3}> */}
+        <Grid item xs={12} md={6}>
+          <p className="copyright">© 2021 Grapeslab. All rights reserved.</p>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <a>
+            <img className="creditcard" src="./cc1.png"></img>
+          </a>
         </Grid>
       </Grid>
+      {/* </Grid> */}
     </div>
   );
 }
