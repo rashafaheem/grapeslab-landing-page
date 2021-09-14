866b3a70ed1c5fbb95febaed80c42708a4d0c223
Rasha Faheem <64769011+rashafaheem@users.noreply.github.com>
2021-09-14 06:58:09 -0700

    fix: testimonial

--- components/testimonial/reviewCard.js
+++ components/testimonial/reviewCard.js
@@ -3,46 +3,50 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import StarBorderIcon from "@material-ui/icons/StarBorder";
 import StarIcon from "@material-ui/icons/Star";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { useTheme } from "@material-ui/styles";
+import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   reviewPaper: {
-    height: "280px",
-    width: "420px",
-    marginLeft: "30px",
+    height: "auto",
+    width: "100%",
+    marginLeft: "20px",
+    marginRight: "20px",
     borderRadius: "6%",
     margin: "auto",
-    [theme.breakpoints.down("sm")]: {
-      width: "340px",
-      height: "300px",
-    },
   },
 }));
 
 function ReviewCard(props) {
   const classes = useStyles();
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up("sm"));
   return (
     <div>
-      <Card className={classes.reviewPaper} elevation={3}>
-        <div className="reviewContents">
-          <p className="review">{props.review}</p>
-          <img className="clientDP" src={props.clientDP}></img>
-          <div className="clientDetailsDiv">
-            <div className="starts">
-              <StarIcon className="starIcon" />
-              <StarIcon className="starIcon" />
-              <StarIcon className="starIcon" />
-              <StarIcon className="starIcon" />
-              <StarBorderIcon className="starIcon" />
-            </div>
-            <h3 className="clientName">{props.clientName}</h3>
-            <p className="clientPlace">{props.clientPlace}</p>
+      <Grid container>
+        <Card className={classes.reviewPaper} elevation={3}>
+          <div className="reviewContents">
+            <Grid item xs={12}>
+              <p className="review">{props.review}</p>
+            </Grid>
+            <Grid item xs={4} className="clientDPGrid">
+              <img className="clientDP" src={props.clientDP}></img>
+            </Grid>
+            <Grid item xs={8} className="clientDetailsGrid">
+              <Grid item xs={12}>
+                <StarIcon className="starIcon" />
+                <StarIcon className="starIcon" />
+                <StarIcon className="starIcon" />
+                <StarIcon className="starIcon" />
+                <StarBorderIcon className="starIcon" />
+              </Grid>
+              <Grid item xs={12}>
+                <h3 className="clientName">{props.clientName}</h3>
+              </Grid>
+              <Grid item xs={12}>
+                <p className="clientPlace">{props.clientPlace}</p>
+              </Grid>
+            </Grid>
           </div>
-        </div>
-      </Card>
+        </Card>
+      </Grid>
     </div>
   );
 }
--- components/testimonial/testimonial.js
+++ components/testimonial/testimonial.js
@@ -3,8 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import ReviewCard from "./reviewCard.js";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { useTheme } from "@material-ui/styles";
 
 const useStyles = makeStyles((theme) => ({
   testimonialCard: {
@@ -12,27 +10,14 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     height: "100%",
     marginTop: "90px",
-    marginLeft: "140px",
+    marginLeft: "20px",
+    marginRight: "20px",
     marginBottom: "50px",
-    [theme.breakpoints.down("md")]: {
-      marginLeft: "50px",
-    },
-  },
-
-  reviewDiv: {
-    paddingLeft: "200px",
-    marginTop: "-80px",
-    gap: "40px",
-    [theme.breakpoints.down("md")]: {
-      marginLeft: "-220px",
-    },
   },
 }));
 
 function Testimonial() {
   const classes = useStyles();
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up("sm"));
   return (
     <div>
       <Grid container>
@@ -48,26 +33,32 @@ function Testimonial() {
           </Card>
         </Grid>
       </Grid>
-      <div className={classes.reviewDiv}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} lg={6}>
-            <ReviewCard
-              review="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-              clientDP="./dp1.jpg"
-              clientName="Micheal Joy Das"
-              clientPlace="San Jose, South Dakota 83475"
-            />
-          </Grid>
-          <Grid item xs={12} lg={6}>
-            <ReviewCard
-              review="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-              clientDP="./dp2.jpg"
-              clientName="Jerome Bell"
-              clientPlace="Gray St. Utica, Pennsylvania 57867"
-            />
-          </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={4}>
+          <ReviewCard
+            review="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+            clientDP="./dp1.jpg"
+            clientName="Micheal Joy Das"
+            clientPlace="San Jose, South Dakota 83475"
+          />
         </Grid>
-      </div>
+        <Grid item xs={12} md={4}>
+          <ReviewCard
+            review="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+            clientDP="./dp2.jpg"
+            clientName="Jerome Bell"
+            clientPlace="Gray St. Utica, Pennsylvania 57867"
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <ReviewCard
+            review="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+            clientDP="./dp1.jpg"
+            clientName="Micheal Joy Das"
+            clientPlace="San Jose, South Dakota 83475"
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 }
